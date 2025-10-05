e82660ad2c633b608ff98fecd43bd2005191b998
iamukul23 <cloudmukul@gmail.com>
2025-10-05 23:20:52 +0530

    first commit

--- client/chat-app/src/App.jsx
+++ client/chat-app/src/App.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import {io} from 'socket.io-client'
+const socket =io.connect("http://localhost:3000");
 const App = () => {
+  
+  const [input,setInput]=useState("");
+  const [messages,setMessages]=useState("");
+
+  const handlemessage= () =>{
+  
+    socket.emit("send_message",{msg: input});
+    setInput("");
+
+  }
+  useEffect(()=>{
+   socket.on("receive_message",(data)=>{
+   setMessages(data);
+   })
+  },[]);
+
+ 
   return (
     <div>
-     <button>send</button>
+      <input type="text"  placeholder='Enter your msg '
+      onChange={(e)=>setInput(e.target.value)} value={input}/>
+     <button onClick={handlemessage} >send</button>
+     <div>
+   {messages}
+     </div>
     </div>
   )
 }
+
 
 export default App
--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const http = require("http"); // <-- FIXED
 const { Server } = require("socket.io");
+const { Socket } = require("dgram");
+const { disconnect } = require("process");
 
 const app = express();
 app.use(cors());
@@ -14,6 +16,17 @@ const io = new Server(server, {
     methods: ["GET", "POST"],
   },
 });
+io.on("connection",(socket)=>{
+  console.log(`user connected:  ${socket.id} `); 
+
+  socket.on("send_message",({msg})=>{
+    console.log(msg);
+    socket.emit("receive_message",msg)
+  })
+  socket.on("disconnect",()=>{
+     console.log(`user disconnected:  ${socket.id} `);
+  })
+})
 
 server.listen(3000, () => {
   console.log("server is running on port 3000");
